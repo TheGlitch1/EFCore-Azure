984cff40a8bca987f73064b8508eeb12e52ab012
yarraf <arraf.youness@gmail.com>
2020-06-25 15:35:30 +0100

    add connection string from sql database azure

--- EFCoreTuto/EFCoreContext.cs
+++ EFCoreTuto/EFCoreContext.cs
@@ -11,7 +11,7 @@ namespace EFCoreTuto
         public DbSet<Tag> Tags { get; set; }
         public DbSet<Attributes> Attributes { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlServer("");
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlServer("Server=tcp:twglnjsx61.database.windows.net,1433;Initial Catalog=EFCoreTutoDB;Persist Security Info=False;User ID=yar;Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
         
     }
 }
