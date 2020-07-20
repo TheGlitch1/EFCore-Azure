114b3706c31c1424a0902d4f5c7b7444520a18c3
yarraf <arraf.youness@gmail.com>
2020-07-20 15:16:14 +0100

    initi repository and project

---
+++ EFCoreContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using EFCoreTuto.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace EFCoreTuto
+{
+    public class EFCoreContext: DbContext
+    {
+
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Attributes> Attributes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlServer("Server=tcp:twglnjsx61.database.windows.net,1433;Initial Catalog=EFCoreTutoDB;Persist Security Info=False;User ID=yar;Password=P@ssw0rd;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EFCoreTuto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            using(EFCoreContext context = new EFCoreContext())
+            {
+                Console.WriteLine(context.Database.CanConnect());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
