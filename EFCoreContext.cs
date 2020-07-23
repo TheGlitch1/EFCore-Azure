7cec073262ed0d587ea544917b71384331ae0f5f
yarraf <arraf.youness@gmail.com>
2020-07-23 12:45:53 +0100

    intégrate user secret

--- EFCoreContext.cs
+++ EFCoreContext.cs
@@ -13,6 +13,6 @@ namespace EFCoreTuto
         public DbSet<Tag> Tags { get; set; }
         public DbSet<Attributes> Attributes { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlServer("Server=tcp:twglnjsx61.database.windows.net,1433;Initial Catalog=EFCoreTutoDB;Persist Security Info=False;User ID=yar;Password=P@ssw0rd;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");             
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlServer(this._secret.ConnectionString);             
     }
 }
